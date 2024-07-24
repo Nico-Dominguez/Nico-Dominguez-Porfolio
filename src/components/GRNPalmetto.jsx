c292264e77d3abb34521873ee53af0ae2903f745
Nico <130573301+Nico-Dominguez@users.noreply.github.com>
2024-07-24 11:16:27 -0400

    remove uneeded dependencies

--- src/components/GRNPalmetto.jsx
+++ src/components/GRNPalmetto.jsx
@@ -1,7 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
 
 const GRNPalmetto = () => {
